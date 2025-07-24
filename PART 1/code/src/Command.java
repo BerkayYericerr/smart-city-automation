a7082187cf5e980381e2544bcd5ebb789fe6d8cd
BerkayYericerr <99441487+BerkayYericerr@users.noreply.github.com>
2025-07-24 14:02:30 +0300

    Add files via upload

---
+++ PART 1/code/src/Command.java
@@ -0,0 +1,28 @@
+//This is the implementation of the Command Pattern as suggested in Homework1.
+
+
+interface Command {
+    void execute();
+}
+class TurnOffStreetLightsCommand implements Command {
+    private CityController cityController;
+    public TurnOffStreetLightsCommand(CityController cityController) {
+        this.cityController = cityController;
+    }
+    @Override
+    public void execute() {
+        this.cityController.controlStreetLights("OFF"); // Turns off the streetlights
+    }
+}
+//  Command to turn ON streetlights
+// Command  action to CityController
+class TurnOnStreetLightsCommand implements Command {
+    private CityController cityController;
+    public TurnOnStreetLightsCommand(CityController cityController) {
+        this.cityController = cityController;
+    }
+    @Override
+    public void execute() {
+        this.cityController.controlStreetLights("ON"); // Turns on the streetlights
+    }
+}
